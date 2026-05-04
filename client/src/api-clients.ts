d5fa6791a801a358f7eab9270d8fe6ced4911ef5
Svoldgaard <Svoldgaard96@gmail.com>
2026-05-04 10:06:31 +0200

    api and client made ready for coding

---
+++ client/src/api-clients.ts
@@ -0,0 +1,23 @@
+//import { TOKEN_KEY, tokenStorage } from "./Atoms/Auth.ts";
+//import { AuthClient, CommandClient, WebClientClient} from "./generated-ts-client.ts";
+
+export const customFetch = async (url: RequestInfo, init?: RequestInit) => {
+    const token = tokenStorage.getItem(TOKEN_KEY, null);
+
+    if (token) {
+        init = {
+            ...(init ?? {}),
+            headers: {
+                ...(init?.headers ?? {}),
+                "Authorization": `Bearer ${token}`,
+            },
+        };
+    }
+    return await fetch(url, init);
+};
+
+export const apiUrl = import.meta.env.VITE_API_URL;
+
+export const authClient = new AuthClient(apiUrl, { fetch: customFetch });
+export const commandClient = new CommandClient(apiUrl, {fetch: customFetch});
+export const webClient = new WebClientClient(apiUrl, { fetch: customFetch });
---
+++ server/api/Controller/GameConsoleController.cs
@@ -0,0 +1,6 @@
+namespace api.Controller;
+
+public class GameConsoleController
+{
+    
+}
